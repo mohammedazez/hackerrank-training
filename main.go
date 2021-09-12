299513a1d59ef82983b7fb62386149de3f753c17
mohammedazez <muhamadaziz047@gmail.com>
2021-09-12 21:59:32 +0700

    add simple array sum

--- main.go
+++ main.go
@@ -2,19 +2,8 @@ package main
 
 import "fmt"
 
-func simpleArraySum(ar []int32) int32 {
-	// Write your code here
-	var sum int32
-
-	for i, s := range ar {
-		fmt.Println(i, s)
-		sum += s
-	}
-	return sum
-}
-
 func main() {
+	// simpleArraySum
 	primes := []int32{1, 2, 3, 4, 10, 11}
-
 	fmt.Println(simpleArraySum(primes))
 }
---
+++ simpleArraySum.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func simpleArraySum(ar []int32) int32 {
+	// Write your code here
+	var sum int32
+
+	for i, s := range ar {
+		fmt.Println(i, s)
+		sum += s
+	}
+	return sum
+}
